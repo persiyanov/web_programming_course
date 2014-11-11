1a550a11eb5fc4f68ffeea9c40876e64cca66e36
drack3800 <domaso@mail.ru>
2014-11-11 21:09:51 +0300

    Added all logic for 2nd task exclude LocalStorage saving. Will be soon.

--- todolist/index.js
+++ todolist/index.js
@@ -1,7 +1,7 @@
 $(function () {
 	// Task deleting
     $(document).on('click', '.delete-task', function (e) {
-		$(this).parent('.task').css('display', 'none');
+		$(this).parent('.task').remove();
         var totalSpanTag = $('.total>span');
         var oldValue = +(totalSpanTag.text());
         totalSpanTag.text(oldValue - 1);
@@ -11,7 +11,7 @@ $(function () {
     $('#tasks-adding').submit(function () {
         var input = $('#task-descr');
         if (input.val() !== "") {
-            $('#tasks').append('<div class="task"><input type="checkbox"><span> ' +
+            $('#tasks').append('<div class="task"><input type="checkbox"><span>' +
                                input.val() + '</span><button class="delete-task">X</button>');
             input.val("");
             var totalSpanTag = $('.total>span');
@@ -46,4 +46,42 @@ $(function () {
     $('.delete-all').click(function () {
         $('.delete-task').click(); 
     });
+    
+    // Task editing
+    $('#tasks').on('dblclick', '.task>span', function () {
+        var $span = $(this);
+        var spanText = $span.text();
+        var $form = $('<form class="pure-form edition-form">');
+        var $input = $('<input type="text">').val(spanText);
+        $form.append($input);
+        $span
+            .text("")
+            .append($form);
+        $input.select();
+    });
+    
+    function replaceInputWithText($form) {
+        $input = $form.find('input').first();
+        var newText = $input.val();
+        $span = $form.parent();
+        $form.remove();
+        $span.text(newText);
+        return false;
+    }
+    
+    $(document).on('submit', 'form[class*=edition-form]', function () {
+        replaceInputWithText($(this));
+        return false;
+    });
+    
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('form[class*=edition-form').length) {
+            replaceInputWithText($(this).find('form[class*=edition-form').first());
+        }
+    });
+    
+//    $(document).on('blur', 'form[class*=edition-form]>input', function() {
+//        replaceInputWithText($(this).parent()); 
+//        return false;
+//    });
 });
