c229a1b0f5797f14af5920caee2415fd8e924cae
drack3800 <domaso@mail.ru>
2014-11-11 03:26:56 +0300

    Added some logic and pretty elements. LocalStorage will be soon

--- todolist/logic.js
+++ todolist/logic.js
@@ -1,38 +1,49 @@
-$(document).ready(function() {
-	$(document).on('click', '.delete-task', function(e) {
+$(function () {
+	// Task deleting
+    $(document).on('click', '.delete-task', function (e) {
 		$(this).parent('.task').css('display', 'none');
-	})
-
-	$(document).on('click', 'input[type=checkbox]', function(e) {
-		var input = $(this);
-		if (input.prop('checked') == true) {
-			input.siblings('span').css('text-decoration', 'line-through');			
-		} else {
-			input.siblings('span').css('text-decoration', 'none');
-		}
-	})
-
-	$('#add-task').click(function(e) {
-		var input = $(this).siblings('input');
-		var input_val = input.val();
-		if (input_val != "") {
-			$('#tasks').append('<div class="task"><input type="checkbox"><span> ' + 
-				input_val + '</span><button class="delete-task">X</button>');
-			input.val("");
-		}
-		
-	})
-
-	$('#task-descr').keypress(function(e) {
-		if (e.which == 13) {
-			e.preventDefault();
-			var input_val = $(this).val();
-			if (input_val != "") {
-				$('#tasks').append('<div class="task"><input type="checkbox"><span> ' + 
-					input_val + '</span><button class="delete-task">X</button>');
-				$(this).val("");
-			}
-			return false;
-		}
-	})
+        var totalSpanTag = $('.total>span');
+        var oldValue = +(totalSpanTag.text());
+        totalSpanTag.text(oldValue - 1);
+	});
+    
+    // Task adding
+    $('#tasks-adding').submit(function () {
+        var input = $('#task-descr');
+        if (input.val() !== "") {
+            $('#tasks').append('<div class="task"><input type="checkbox"><span> ' +
+                               input.val() + '</span><button class="delete-task">X</button>');
+            input.val("");
+            var totalSpanTag = $('.total>span');
+            var oldValue = +(totalSpanTag.text());
+            totalSpanTag.text(oldValue + 1);
+        }
+        return false;
+    });
+    
+    // Line-through and increment accomplished tasks value
+    $(document).on('click', '.task>input:checkbox:checked', function () {
+        $(this).siblings('span').first().css('text-decoration', 'line-through');
+        var accomplishedSpanTag = $('.accomplished>span');
+        var oldValue = +(accomplishedSpanTag.text());
+        accomplishedSpanTag.text(oldValue + 1);
+    });
+    
+    // Reverse actions to previous function
+    $(document).on('click', '.task>input:checkbox:not(:checked)', function () {
+        $(this).siblings('span').first().css('text-decoration', 'none');
+        var accomplishedSpanTag = $('.accomplished>span');
+        var oldValue = +(accomplishedSpanTag.text());
+        accomplishedSpanTag.text(oldValue - 1);
+    });
+    
+    // Marking all as done
+    $('.mark-all').click(function () {
+         $('.task>input:checkbox').click();
+    });
+    
+    // Deleting all tasks
+    $('.delete-all').click(function () {
+        $('.delete-task').click(); 
+    });
 });
